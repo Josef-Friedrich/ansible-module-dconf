d5da63b8eb774abd1c46e2b75c02cdda665e88f7
Josef Friedrich <josef@friedrich.rocks>
2017-06-05 22:37:58 +0200

    Implement integer and boolean values

--- dconf.py
+++ dconf.py
@@ -7,6 +7,22 @@ import gi
 from gi.repository import Gio, GLib
 gi.require_version('Gtk', '3.0')
 
+def g_variant(value):
+    try:
+        value = int(value)
+        return GLib.Variant('i', value)
+    except ValueError:
+        pass
+    if isinstance(value, str):
+        if value.lower() == 'no' or value.lower() == 'false':
+            return GLib.Variant('b', False)
+        elif value.lower() == 'yes' or value.lower() == 'true':
+            return GLib.Variant('b', True)
+        else:
+            return GLib.Variant('s', value)
+    elif isinstance(value, list):
+        return GLib.Variant('as', value)
+
 
 def set_env():
     env = 'DBUS_SESSION_BUS_ADDRESS'
@@ -32,11 +48,22 @@ def main():
 
     set_env()
 
+    changed = False
     schema = Gio.Settings(p['schema'])
-    schema.set_value(p['key'], GLib.Variant('s', p['value']))
-    output = schema.get_value(p['key'])
+    old_value = schema.get_value(p['key'])
+    new_value = g_variant(p['value'])
 
-    module.exit_json(changed=True, msg=str(output))
+    if old_value != new_value:
+        schema.set_value(p['key'], g_variant(p['value']))
+        changed = True
+        new_value = schema.get_value(p['key'])
+
+    module.exit_json(changed=changed,
+                     key=p['key'],
+                     schema=p['schema'],
+                     old_value_input=p['value'],
+                     old_value=str(old_value),
+                     new_value=str(new_value))
 
 
 if __name__ == '__main__':
